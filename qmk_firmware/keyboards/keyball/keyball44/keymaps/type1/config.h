07022dd1e26cc8c6dcdac4ecc066b01a29be22af
wataruw <saruchichi@gmail.com>
2025-03-02 10:16:22 +0900

    Keyballが反転したので、以下の2.で対応
    この現象は、Keyball44用のファームウェアがもともと QMK 0.22.14 向けに設計されているため、最新の QMK Firmware（たとえば 0.24.x 以降）でビルドすると、左右のマトリクスが反転してしまう既知の問題です。GitHubのディスカッション（ ）でも、正規のファームウェアは QMK 0.22.14 を想定していると説明されており、また最新QMKを使う場合はPR #496で対処可能との報告があります。
    
    具体的な解決策としては以下の2つが考えられます：
    	1.	QMK 0.22.14 でビルドする
    　　Keyball44の公式ビルド手順に従い、推奨される QMK バージョンでコンパイルすることで、左右反転の問題は発生しません。
    	2.	最新QMKを使う場合、設定の修正を行う
    　　config.h にたとえば #define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT のような定義を追加することで、左右のマトリクス判定を正しく行えるようにする方法も報告されています。REMAPPツールで書き込んだファームウェアでは正常な動作をしていることから、ハードウェア自体は問題なく、あくまでファームウェア側の設定の差異によるものと考えられます。
    
    以上のいずれかの方法で対応すると、キーの反転現象は解消されるはずです。

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/config.h
@@ -37,3 +37,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
