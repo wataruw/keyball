88484f497c502b105d49bd95ad73d35dc7deb2c9
wataruw <saruchichi@gmail.com>
2025-03-04 17:46:42 +0900

    twpair_on_jisは薙刀式では未使用だったので削除した

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/keymap.c
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "keymap_japanese.h"
-#include "twpair_on_jis.h"
 #include "naginata.h"
 
 // 薙刀式のオン/オフに使うキーの定義
@@ -32,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_ESC   ,
-              KC_LNG2, KC_LNG1, KC_LGUI                  , KC_SPC , MO(2),                       LT(1,KC_BTN1), KC_SPC, _______,     _______  , LT(3,KC_BTN2)
+              KC_LNG2, KC_LNG1, KC_LGUI                  , KC_SPC , MO(2),                       LT(1,KC_BTN1), KC_SPC, _______          , _______  , LT(3,KC_BTN2)
   ),
 
   [1] = LAYOUT_universal(
@@ -54,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , NG_Q     , NG_W     , NG_E     , NG_R     , NG_T     ,                                        NG_Y     , NG_U     , NG_I     , NG_O     , NG_P     , KC_BSPC  ,
     _______  , NG_A     , NG_S     , NG_D     , NG_F     , NG_G     ,                                        NG_H     , NG_J     , NG_K     , NG_L     , NG_SCLN  , KC_ENT   ,
     _______  , NG_Z     , NG_X     , NG_C     , NG_V     , NG_B     ,                                        NG_N     , NG_M     , NG_COMM  , NG_DOT   , NG_SLSH  , KC_ESC   ,
-              KC_LNG1, KC_LNG2, KC_LGUI                  , NG_SHFT  , _______  ,                   _______  , NG_SHFT2   , _______        , _______  , _______
+              KC_LNG1, KC_LNG2, KC_LGUI                  , NG_SHFT  , MO(2)    ,                 LT(1,KC_BTN1), NG_SHFT2 , _______       , _______  , LT(3,KC_BTN2)
   ),
 };
 // clang-format on
@@ -69,11 +68,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_naginata(keycode, record)) {
         return false;
     }
-    
-  // JIS配列に変換
-  // if (!twpair_on_jis(keycode, record)) {
-  //   return false;
-  // }
 
   return true;
 }
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/type1/rules.mk
@@ -4,13 +4,4 @@ OLED_ENABLE = yes
 UNICODE_ENABLE = yes
 
 # Add naginata support
-SRC += twpair_on_jis.c
 SRC += naginata_v15.c
-
-LTO_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
-EXTRAKEY_ENABLE = no
-GRAVE_ESC_ENABLE = no
-JAPANESE_ENABLE = yes
